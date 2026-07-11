cb73f8d54e5dd4d9aa05344e16936f2b29dea701
Gopal <madgamgopal73860@gmail.com>
2026-07-11 23:44:20 +0530

    Day 1

---
+++ Count_digits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Count_digits {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    // int count = 0;
+    // if(n == 0) {
+    //   System.out.println(1);
+    // }
+    // while(n > 0) {
+    //   count++;
+    //   n /= 10;
+    // }
+    // System.out.println(count);
+
+    System.out.println((int)Math.floor(Math.log10(n) + 1));
+  }
+  
+}
