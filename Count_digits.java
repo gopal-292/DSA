0c66ffcde4ef840f1b0f8bf39743d424c40b02bc
Gopal <madgamgopal73860@gmail.com>
2026-07-21 08:05:44 +0530

    Day 2

--- Count_digits.java
+++ Count_digits.java
@@ -3,15 +3,15 @@ class Count_digits {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    // int count = 0;
-    // if(n == 0) {
-    //   System.out.println(1);
-    // }
-    // while(n > 0) {
-    //   count++;
-    //   n /= 10;
-    // }
-    // System.out.println(count);
+    int count = 0;
+    if(n == 0) {
+      System.out.println(1);
+    }
+    while(n > 0) {
+      count++;
+      n /= 10;
+    }
+    System.out.println(count);
 
     System.out.println((int)Math.floor(Math.log10(n) + 1));
   }
