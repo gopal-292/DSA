a51e467203f65865adb74a8ec42ff036dce827c7
Gopal <madgamgopal73860@gmail.com>
2026-07-30 07:14:12 +0530

    Day 7

---
+++ Desktop/dsa/Basics/Strings/LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+package Basics.Strings;
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[]  str = {"flowers" , "flow" , "fly", "flight" };
+        int n = str.length;
+        if(n == 0) {
+            System.out.println("");
+            return;
+        }
+        String word = str[0];
+        for(int i = 0;i<word.length();i++) {
+            char ch = word.charAt(i);
+            for(String s : str) {
+                if(s.length() <= i) {
+                    System.out.println(s);
+                    return;
+                }
+                if(s.charAt(i) != ch) {
+                    System.out.println(s.substring(0,i));
+                    return;
+                }
+            }
+        }
+        System.out.println(word);
+    }
+}
